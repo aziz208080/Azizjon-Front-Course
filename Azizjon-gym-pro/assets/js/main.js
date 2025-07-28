be50ab114daab87d79595647d187e46965cdeca7
SONICKOMIL <rshamsidinova@gmail.com>
2025-07-28 17:23:52 +0500

    28.07.2025

--- Azizjon-gym-pro/assets/js/main.js
+++ Azizjon-gym-pro/assets/js/main.js
@@ -43,9 +43,35 @@ window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/
 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
@@ -133,7 +159,17 @@ const sendEmail = (e) => {
                 // Show message and add color
                 contactMessage.classList.add('color-green')
                 contactMessage.textContent = 'You registered successfully 💪'
+                
+                // Remove message three seconds
+                setTimeout(() =>{
+                    contactMessage.textContent = ''
+                },  3000)
+            }, (error) =>{
+                // Mail sending error
+                alert('OOPS! SOMETHING HAS FAILED...', error)
             })
+        // To clear the input field
+        contactUser.value = ''
     }
 }
 
