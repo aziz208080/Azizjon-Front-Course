0c0bc223d2737f3af50ab1d6cbe9e63c13253891
aziz208080 <a16135516@gmail.com>
2025-11-18 22:35:21 +0500

    18.11.2025

---
+++ js-ex/03/006-7-8-9-10-11.js
@@ -0,0 +1,21 @@
+let a 
+
+const c = 10    
+
+// a = true 
+
+console.log(a)
+
+let myName
+
+console.log(myName)
+
+myName = 'Aziz'
+
+console.log(myName) 
+
+console.log(console.log(10))
+
+const myNamee = 'Aziz'
+
+console.log(myNamee)
---
+++ js-ex/03/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let a 
+
+const c = 10    
+
+// a = true 
+
+console.log(a)
