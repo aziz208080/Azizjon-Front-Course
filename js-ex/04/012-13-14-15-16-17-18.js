9eacfbf6575409e033cc476bf867289c88abed62
aziz208080 <a16135516@gmail.com>
2025-12-06 17:55:07 +0500

    06.12.2025

--- js-ex/04/012-13-14-15-16-17-18.js
+++ js-ex/04/012-13-14-15-16-17-18.js
@@ -1,73 +1,108 @@
-const myCit = {
-    city:'New York',
-    popular: true,
-    country: 'USA' 
-}
+// const myCity = {
+//     city: 'New York',
+//     popular: true,
+//     country: 'USA'
+// }
 
-console.log(myCit.city);
+// console.log(myCity.city);
+// console.log(myCity['city']);
 
-console.log(myCit.popular);
-
-
+// console.log(myCity.popular);
 
 
 // ex-13
 
-const myCity = {
-    city: 'New York'
-}
-myCity.popular = true
+// const myCity = {
+//     city: 'New York'
+// }
+// myCity.popular = true
 
-console.log(myCity);
+// console.log(myCity);
 
-myCity.country = 'USA'
+// myCity.country = 'USA'
 
-console.log(myCity);
+// console.log(myCity);
 
-delete myCity.popular
+// delete myCity.popular
 
-console.log(myCity);
+// console.log(myCity);
 
-myCity['popular'] = true
+// myCity['popular'] = true
 
-const countryPropertyName = 'country'
-myCity[countryPropertyName] = 'USA'
+// const countryPropertyName = 'country'
+// myCity[countryPropertyName] = 'USA'
 
-console.log(myCity); 
+// console.log(myCity);
 
 // ex-14
 
-const myCityy = {
-    city: 'New York',
-    info: {
-        isPopular: true,
-        country: 'USA'
-    }
-}
+// const myCityy = {
+//     city: 'New York',
+//     info: {
+//         isPopular: true,
+//         country: 'USA'
+//     }
+// }
 
-console.log(myCityy)
+// console.log(myCityy)
 
-delete myCityy.info['isPopular']
+// delete myCityy.info['isPopular']
 
-console.log(myCityy);
+// console.log(myCityy);
 
 // ex-15
 
 
-const name = 'Aziz'
-const postsQty = 23
+// const name = 'Aziz'
+// const postsQty = 23
 
-const userProfile = {
-    name,
-    postsQty,
-    hasSignedAgreement: false
-}
+// const userProfile = {
+//     name,
+//     postsQty,
+//     hasSignedAgreement: false
+// }
 
-console.log(userProfile);
-
-
+// console.log(userProfile);
 
 
+// ex-16
 
 
+// globalThis
+
+
+// ex-17
+
+
+// const myCity = {
+//     city: 'New York',
+//     citiGreeting: function () {
+//         console.log('Greeting');
+//     }
+// }
+
+// const post = {
+//     title: 'my post',
+//     likesQty:5
+// }
+
+// console.log(post);
+
+
+// myCity.citiGreeting()
+
+// console.log(myCity);
+
+
+// ex-18
+
+
+// {
+//     "userId": 1,
+//     "id": 1,
+//     "title": "Test title",
+//     "status": {
+//         "completed": false,
+//     }
+// }
 
