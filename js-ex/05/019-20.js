10b3ede07216f634551a9fc69352786a1ebef90a
aziz208080 <a16135516@gmail.com>
2026-01-06 15:43:24 +0500

    06.01.2026

---
+++ js-ex/05/019-20.js
@@ -0,0 +1,98 @@
+// const a = 10    
+// let b = a 
+// b = 30
+
+// console.log(b);
+
+
+// const person = {
+//     name: 'bob',
+//     age: 21
+// }
+
+// person.age = 22
+// person.isAdult = true
+
+// console.log(person);
+
+
+// const person = {
+//     name: 'bob',
+//     age: 25
+// }
+// const person2 = person
+
+// person2.age = 26
+// person.isAdult = true
+
+// console.log(person.age);
+// console.log(person2.isAdult);
+// console.log(person);
+// console.log(person2);
+
+
+// 20-ex
+
+
+//  const person = {
+//     name: 'bob',
+//     age: 25,
+//     students: {
+//         alice: {
+//             name: 'ALICE', 
+//             age: 20,
+//         },
+//         bob: {
+//             name: 'BOB', 
+//             age: 22,
+//         },
+//         alex: {
+//             name: 'Alex', 
+//             age: 18,
+//         },
+//     },
+//  }
+
+//  const person2 = Object.assign({}, person)
+//  person2.age = 26
+//  person.isAdult = true
+
+//  person2.students.alice.name = 'John'
+//  console.log(person2.age);
+//  console.log(person);
+// console.log(person2);
+
+
+
+// const person = {
+//     name: 'bob',
+//     age:25
+// }
+
+// const person2 = {...person}
+
+// person2.name = 'Alise'
+
+// console.log(person2.name);
+ 
+
+
+//Destruction object and array
+
+// const person = {
+//     name: 'bob',
+//     age: 25
+// }
+ 
+// const  {name, age}  = person
+
+
+// console.log(name, age);
+
+
+
+// let arr = [1, 2, 3]
+
+// let [indx0, indx1, indx2 ] = arr
+
+// console.log(indx2, indx0, indx1);
